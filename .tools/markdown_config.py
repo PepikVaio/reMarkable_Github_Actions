7f7a5bd372ecf4b53ed258c5c34bedbd571047cf
Wajsar Josef <36114753+PepikVaio@users.noreply.github.com>
2026-07-12 18:30:48 +0200

    Create markdown_config.py

---
+++ .tools/markdown_config.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+config_file = sys.argv[1]
+
+if not os.path.exists(config_file):
+    raise Exception(f"Config file not found: {config_file}")
+
+with open(config_file, encoding="utf-8") as file:
+
+    for line in file:
+        line = line.strip()
+
+        if not line:
+            continue
+
+        if line.startswith("#"):
+            continue
+
+        key, value = line.split("=", 1)
+
+        with open(
+            os.environ["GITHUB_ENV"],
+            "a",
+            encoding="utf-8"
+        ) as env:
+
+            env.write(f"{key}={value}\n")
+
+print("Configuration loaded")
